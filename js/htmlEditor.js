d4df9cb5c1eb22f5b24356ae34506e86a3e0637f
Olivier Girardot <ogmusik@gmail.com>
2020-09-11 11:06:55 +0200

    not sure I got the point here

--- js/htmlEditor.js
+++ js/htmlEditor.js
@@ -2,9 +2,15 @@ function htmlEditor () {
     return {
         require: 'ngModel',
         link: function ($scope, $element, $attrs, $ctrl) {
+            // view -> Model
             $element.on('input', function (event) {         // event listener on element // input referes to the input initiated by "contenteditable"
-                $ctrl.$setViewValue(event.target.innerHTML);    //  we are setting a view value
+                $ctrl.$setViewValue(event.target.innerHTML);    //  we are setting a view value, $ctrl is the default model controller linked to the ngModel that we have declared in the index.html
             });
+            // Model -> View
+            $ctrl.$render = function () {
+                $element.html($ctrl.$modelValue); // $element.html is the jquery way of writting: $element[0].innerHTML 
+            };
+            $ctrl.$setViewValue($element[0].innerHTML);
         }
     };
 };
